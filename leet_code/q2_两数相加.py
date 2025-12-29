105ea6ba7d1b7fef8d2c3369781eb779438d4c37
vv <math89423@gmail.com>
2025-12-29 15:39:36 +0800

    update

--- leet_code/q2_两数相加.py
+++ leet_code/q2_两数相加.py
@@ -36,12 +36,62 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        pass
-
-    def create_linked_list(arr):
-        
+        add_num1 = self.caculate_add_sum(l1)
+        add_num2 = self.caculate_add_sum(l2)
+        #print(f"add_num1:{add_num1}  add_num2:{add_num2}")
+        sum = add_num1 + add_num2
+        #print(f"sum:{sum}")
+        # 求余数做数组
+        if sum == 0:
+            res_link = ListNode(0)
+            return res_link
+        res = []
+        while sum != 0:
+            res.append(sum % 10)    # 百分号%是取余数
+            sum = sum // 10         # //代表整除
+            res_link = self.create_linked_list(res)
+        return res_link
+    
+    def create_linked_list(self, arr):
+        # 定义哑结点，便于链表操作
         dummy = ListNode(0)
+        current = dummy
+        for val in arr:
+            # 每读到一个数字，就建立一个节点
+            current.next = ListNode(val)
+            # 指针向后移动一步，站在新节点
+            current = current.next
+        # dummy是虚假的链表头，dummy.next才是真正的链表头
+        return dummy.next
 
+    def print_linked_list(self, head):
+        result = []
+        current = head
+        while current:
+            result.append(current.val)
+            current = current.next
+        return result
+
+    def caculate_add_sum(self, caculate_list):
+        # TODO:取值，取值系数，加数
+            current = caculate_list
+            sum = 0     # 求加数1的值，起始值为0
+            sqar_num = 1    # 代表乘积系数，头结点是1，后面一个节点依次扩大10倍
+            while current:
+                # num代表当前位数值
+                num = current.val
+                current = current.next
+                add_num = num * sqar_num
+                sum += add_num
+                sqar_num *= 10
+            return sum
 
 if __name__ == '__main__':
-    pass
+    arr1 = [0]
+    arr2 = [0]
+    solution = Solution()
+    l1 = solution.create_linked_list(arr1)
+    l2 = solution.create_linked_list(arr2)
+    res = solution.addTwoNumbers(l1, l2)
+
+    print(solution.print_linked_list(res))
